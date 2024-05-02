8f3f5e1acaafa92bbd2fb900c715f3ba1f585487
Dylan-Carle <dylancarle2005@hotmail.com>
2024-05-02 16:49:13 -0400

    ajout des flches + amelioration de single.php

--- single.php
+++ single.php
@@ -11,22 +11,25 @@
                 while(have_posts()): the_post();
                 //strpos();
                 ?>
-                <div class="carte">
+                <div class="carte__single">
                     <h3><?php the_title(); ?></h4>
-                    <?php the_post_thumbnail("medium"); ?>
                     <p><?php the_content(); ?></p>
+                    <p>Temperature Minimum : <?php the_field("temperature_minimum") ?></p>
+                    <p>Temperature Maximum : <?php the_field("temperature_maximum") ?></p>
+                    <p>Ville Avoisinante : <?php the_field("villes_avoisinantes") ?></p>
+                    <p>Humidité : <?php the_field("humidite") ?></p>
                 </div>
                 <?php endwhile; ?>
             <?php endif; ?>
         </div>
         </section>
     </div>
-    <div id="evenement" class="global">
+    <!-- <div id="evenement" class="global">
         <section class="evenement__section">
             <h2>evenement</h2>
         </section>
         <?php //get_template_part("gabarit/vague"); ?>
-    </div>
+    </div> -->
 
     <?php get_footer(); ?>
 </body>
