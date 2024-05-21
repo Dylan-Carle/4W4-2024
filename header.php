075c01c604f9697e34f1b2aeaddb646de7b263b3
Dylan-Carle <dylancarle2005@hotmail.com>
2024-05-21 13:00:25 -0400

    Amelioration du carrousel et du rest api

--- header.php
+++ header.php
@@ -15,13 +15,15 @@
     <div id="menu" class="global">
         <header class="entete__menu">
             <!-- Permet de mettre un logo custom a l'aide du customizer -->
-            <?php echo get_custom_logo(); ?> 
+            <div class="logo__entete">
+                <?php echo get_custom_logo(); ?> 
+                <?php get_search_form(); ?>
+            </div>
             <input type="checkbox" id="chk_burger">
             <label id="burger" for="chk_burger">
             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </label>
             <?php wp_nav_menu(array("container"=>"nav")); ?>
-            <?php //get_search_form(); ?>
             <hr>
         </header>
     </div>
